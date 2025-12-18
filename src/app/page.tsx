85703581cf116a012b24ac2b94fe1d859993b774
Adam_442 <aam_442@hotmail.com>
2025-12-18 14:58:01 +0300

    intial coming soon screen.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,64 +1,60 @@
 import Image from "next/image";
+import { LiquidGlassCard } from "@/components/ui/liquid-glass";
+import { Cloud } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <div className="flex min-h-screen p-16 items-start justify-start font-sans bg-black">
+      <main className="relative z-10 items-start">
+        <div className="flex flex-col gap-6">
+          <div className="flex gap-5">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+              src="/KFUPM.svg"
+              alt="KFUPM Logo"
+              width={200}
+              height={100}
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+
+            <p className="text-white font-bold text-2xl">|</p>
+
+            <Image
+              src="/GCloud.svg"
+              alt="Google Cloud Logo"
+              width={200}
+              height={100}
+            />
+          </div>
+
+          <Image
+            src="/Intelligent_Planet_logo.svg"
+            alt="Intelligent Planet Hackathon Logo"
+            width={500}
+            height={250}
+          />
+
+          <p className="text-gray-50 font-bold text-2xl">AI Solutions for an Intelligent Planet.</p>
+
+          <LiquidGlassCard
+            shadowIntensity='xs'
+            borderRadius='32px'
+            glowIntensity='none'
+            className='px-10 py-4 text-white bg-white/8 w-fit'
           >
-            Documentation
-          </a>
+            <div className="relative z-30">
+              <p className="text-gray-50 font-bold text-2xl">Coming Soon.</p>
+            </div>
+          </LiquidGlassCard>
+
         </div>
+
+        <Image
+          src="/Globe-Full.png"
+          alt="Intelligent Planet Globe"
+          width={1500}
+          height={750}
+          className="fixed bottom-0 right-0 -z-10 translate-x-1/3 translate-y-1/2"
+        />
+
       </main>
     </div>
   );
